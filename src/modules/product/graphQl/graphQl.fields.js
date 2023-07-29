8c13ae41a86bbb056e2e355a0f75352e9cea339f
sayedmahmoud223 <sbendary977@gmail.com>
2023-07-30 02:05:37 +0300

    some updates in graphQl 2

--- src/modules/product/graphQl/graphQl.fields.js
+++ src/modules/product/graphQl/graphQl.fields.js
@@ -1,12 +1,17 @@
 import {
+    GraphQLID,
     GraphQLList,
 } from "graphql"
 import { productModel } from "../../../../DB/model/Product.model.js"
 import { productType } from "./graphQl.types.js"
 export let getProducts = {
-    type: new GraphQLList(productType),
-    resolve: async () => {
-        let products = await productModel.find({ isDeleted: false }).populate([
+    type: productType,
+    args:{
+        _id:{type: GraphQLID}
+    } ,
+    resolve: async (parent,args) => {
+        let {_id} = args
+        let products = await productModel.findOne({_id, isDeleted: false }).populate([
             {
                 path:"category",
                 select:"name "
